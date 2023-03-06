6bf32f956531b3f99682f59965304d14a8210c6c
szarina <70153589+szarina@users.noreply.github.com>
2023-03-06 20:20:48 +0600

    little rounting

--- TrueLab6/src/app/app-routing.module.ts
+++ TrueLab6/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {AlbumsComponent} from "./albums/albums.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component : AboutComponent  },
+  {path : 'albums', component: AlbumsComponent},
+  {path: '', redirectTo : 'home', pathMatch : 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
