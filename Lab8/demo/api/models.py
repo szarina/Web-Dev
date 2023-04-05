00173203d02322733f49861f3bb220619ab98384
szarina <zarina.saden@gmail.com>
2023-04-05 15:39:48 +0600

    Created ebv and django little api coding

---
+++ Lab8/demo/api/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField(null=True)
+    count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.category} : {self.name}"
+
+    def to_json(self):
+        return {
+            'id': self.pk,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category': self.category.to_json()
+        }
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.pk,
+            'name': self.name
+        }
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
