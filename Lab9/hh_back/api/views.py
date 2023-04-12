7187fcf86bc806aaf736671a7d49c9c831548f9b
szarina <zarina.saden@gmail.com>
2023-04-12 18:12:07 +0600

    added urls api

--- Lab9/hh_back/api/views.py
+++ Lab9/hh_back/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import path
 
 # Create your views here.
+from . import views
+
+urlpatterns = [
+    path('companies/', views.companies_list, name='list of companies'),
+    path('companies/<int:id>/', views.company_details, name='company details'),
+    path('companies/<int:id>/vacancies/', views.company_vacancies_list, name='list of vacancies in company'),
+    path('vacancies/', views.vacancies_list, name='list of vacancies'),
+    path('vacancies/<int:id>/', views.vacancy_details, name='vacancy details'),
+    path('vacancies/top_ten/', views.top_ten_vacancies_list, name='list of top 10 vacancies'),
+
+]
