01b6ccd4fe548e054484ff519073d48875241bfb
szarina <zarina.saden@gmail.com>
2023-04-12 18:56:00 +0600

    added views

--- Lab9/hh_back/api/urls.py
+++ Lab9/hh_back/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
 
-urlpatterns = [
+from . import views
 
-    path(),
+urlpatterns = [
+    path('companies/', views.companies_list, name='list of companies'),
+    path('companies/<int:id>/', views.company_details, name='company details'),
+    path('companies/<int:id>/vacancies/', views.company_vacancies_list, name='list of vacancies in company'),
+    path('vacancies/', views.vacancies_list, name='list of vacancies'),
+    path('vacancies/<int:id>/', views.vacancy_details, name='vacancy details'),
+    path('vacancies/top_ten/', views.top_ten_vacancies_list, name='list of top 10 vacancies'),
+
 ]
--- Lab9/hh_back/hh_back/urls.py
+++ Lab9/hh_back/hh_back/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
+
 from django.contrib import admin
 from django.urls import path,include
-
+def  mainView(request):
+    return HttpResponse("Hii")
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('' , mainView)
 ]
